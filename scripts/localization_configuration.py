73d0700804bfd2054778b78f1a4b087cd5710067
Matan Eilat <matan.eilat@joytunes.com>
2014-07-23 15:52:37 +0300

    Added the configuration file

---
+++ scripts/localization_configuration.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# This value should be configured with designated localization bundle path.
+LOCALIZATION_BUNDLE_PATH = None
+
+DEFAULT_LANGUAGE_DIRECTORY_NAME = "en.lproj"
+
+
