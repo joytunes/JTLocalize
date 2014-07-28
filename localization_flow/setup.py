36a16b8887a1fcfa8df03fb6f84aaa2bc88e2e5d
Matan Eilat <matan.eilat@joytunes.com>
2014-07-28 20:34:59 +0300

    Moved the scripts directory to localization_flow

---
+++ localization_flow/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jtlocalize',
+      version='0.0.1',
+      description='iOS localization framework',
+      author='JoyTunes',
+      author_email='matan.eilat@joytunes.com',
+      url='https://github.com/joytunes/JTLocalize/',
+      package_dir={'':'scripts'},
+     )
