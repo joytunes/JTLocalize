c6e28cd351fbc1dc4fbc1d66125c92f099c154a4
Yoni Tsafir <yoni@joytunes.com>
2015-05-06 18:17:27 +0300

    Added tests (and infra) for actual localization
    Related to issue #3

--- tests/JTLocalizeTests/JTLocalizeTests.m
+++ tests/JTLocalizeTests/JTLocalizeTests.m
@@ -11,12 +11,59 @@
 
 @interface JTLocalizeTests : XCTestCase
 
+@property (nonatomic) NSString *testBundlePath;
+@property (nonatomic) NSFileHandle *currLangLocalizationFileHandle;
+
 @end
 
 @implementation JTLocalizeTests
 
-- (void)testLocalizeSimpleJTLString {
+- (void)setUp {
+    [super setUp];
+    
+    self.testBundlePath = [NSTemporaryDirectory() stringByAppendingPathComponent:
+                           [NSString stringWithFormat:@"JTLocalizeTestBundle%.0f.bundle",
+                                                      [NSDate date].timeIntervalSince1970]];
+    NSString *currLangDir = [self.testBundlePath stringByAppendingPathComponent:@"en.lproj"];
+    NSString *currLangLocalizationFileName = [[currLangDir stringByAppendingPathComponent:kJTDefaultStringsTableName]
+                                              stringByAppendingPathExtension:@"strings"];
+    
+    [[NSFileManager defaultManager] createDirectoryAtPath:currLangDir withIntermediateDirectories:YES
+                                               attributes:nil error:nil];
+    
+    [[NSFileManager defaultManager] createFileAtPath:currLangLocalizationFileName contents:nil attributes:nil];
+    self.currLangLocalizationFileHandle = [NSFileHandle fileHandleForWritingAtPath:currLangLocalizationFileName];
+}
+
+- (void)tearDown {
+    [self.currLangLocalizationFileHandle closeFile];
+    [JTLocalize setLocalizationBundleToPath:nil stringsTableName:nil];
+    [[NSFileManager defaultManager] removeItemAtPath:self.testBundlePath error:nil];
+    
+    [super tearDown];
+}
+
+- (void)localizeString:(NSString *)key to:(NSString *)value {
+    NSString *line = [NSString stringWithFormat:@"\"%@\" = \"%@\";\n", key, value];
+    [self.currLangLocalizationFileHandle writeData:[line dataUsingEncoding:NSUTF16StringEncoding]];
+}
+
+- (void)finishLocalizing {
+    [self.currLangLocalizationFileHandle closeFile];
+    self.currLangLocalizationFileHandle = nil;
+    
+    [JTLocalize setLocalizationBundleToPath:self.testBundlePath stringsTableName:nil];
+}
+
+- (void)testLocalizeSimpleJTLStringNoTranslation {
     XCTAssertEqualObjects(@"JTL('Bla', 'BlaBla')".stringByLocalizingJTLDirectives, @"Bla");
+}
+
+- (void)testLocalizeSimpleJTLStringWhenThereIsTranslation {
+    [self localizeString:@"Bla" to:@"בלה"];
+    [self finishLocalizing];
+    
+    XCTAssertEqualObjects(@"JTL('Bla', 'BlaBla')".stringByLocalizingJTLDirectives, @"בלה");
 }
 
 @end
