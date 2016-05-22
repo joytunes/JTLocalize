e9689ba7b516ad3712bbcaab9f659d7d8f1cebce
Yoni Tsafir <yoni@joytunes.com>
2016-05-22 10:14:37 +0300

    Releasing 0.1.3 with all changes made for Simply Piano 3.0

--- localization_flow/setup.py
+++ localization_flow/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='jtlocalize',
-      version='0.1.2',
+      version='0.1.3',
       description='iOS localization framework',
       author='JoyTunes',
       author_email='info@joytunes.com',
